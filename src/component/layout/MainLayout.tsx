68cedead765295b76f1a8c680971ba03e3e8da55
chiennv <chiennguyen16112003@gmail.com>
2026-02-10 15:03:33 +0700

    test api

--- src/component/layout/MainLayout.tsx
+++ src/component/layout/MainLayout.tsx
@@ -7,6 +7,7 @@ const { Content } = Layout;
 
 export default function MainLayout() {
   const [collapsed, setCollapsed] = useState(false);
+  console.log(import.meta.env.BE_URL);
 
   return (
     <Layout className="main-layout">
